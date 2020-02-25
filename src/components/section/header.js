1cba368828f10d37f9b3f586bf318fc63fe47d19
Edward Perez <edward.121489@gmail.com>
2020-02-25 20:43:00 +0100

    adjust  padding on welcome header

--- src/components/section/header.js
+++ src/components/section/header.js
@@ -10,14 +10,14 @@ const Header = () => {
 
   return (
     <div className='jumbotron jumbotron-fluid text-center bg-transparent vh-75'>
-      <div className='row justify-content-center'>
+      <div className='row py-4 mt-n4 justify-content-center'>
         <div className='col-sm-12 pt-4'>
           <h3 className='pt-4'>Welcome,</h3>
           <h3>my name is Edward.</h3>
           <h3>I have experience with</h3>
         </div>
-        <div className='col-sm-12 pt-4'>
-          <div className='container pt-4'>
+        <div className='col-sm-12 pb-4'>
+          <div className='container pt-3'>
             { child }
           </div>
         </div>
