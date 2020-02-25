9da6aa72a3c42023dbcb098d82d27e53e76445ff
Edward Perez <edward.121489@gmail.com>
2020-02-25 19:02:53 +0100

    added react hook for performance

--- src/components/section/header.js
+++ src/components/section/header.js
@@ -1,54 +1,29 @@
-import React from 'react'
-import { Jumbotron, Container, Row, Col } from 'react-bootstrap'
-import Typewriter from 'typewriter-effect';
+import React, { useState, useLayoutEffect } from 'react'
+import Typist from '../typist'
 
-const Header = () => (
-  <Jumbotron  className='text-center bg-transparent vh-75' fluid>
-    <Row className='justify-content-center'>
-      <Col sm={12} className='pt-4'>
-        {/* <h1 className='py-4'>I'm Edward</h1> */}
-        <h3 className='pt-4'>Welcome,</h3>
-        <h3 className=''>my name is Edward.</h3>
-        <h3 className=''>I have experience with</h3>
-      </Col>
-      <Col sm={12} md={6}>
-        <Container className='pt-4'>
-          <Typewriter
-            options={{
-              strings: [
-                'Javascript',
-                'REST API',
-                'Postman',
-                'Express', 
-                'GraphQL', 
-                'SQL',  
-                'Sequelize',
-                'Nodejs',  
-                'React',
-                'Axios',
-                'MongoDB',
-                'Gatsby',
-                'Git',
-                'Pug',
-                'Bootstrap'
-              ],
-              cursor: '',
-              autoStart: true,
-              loop: true,
-              wrapperClassName: 'h1 text-primary font-weight-bold'
-            }}
-            onInit={(typewriter) => {
-              typewriter.typeString()
-                .pauseFor(300)
-                .deleteAll()
-                .start();
-            }}
-          />
-        </Container>
-      </Col>
-    </Row>
-  </Jumbotron>
-)
+const Header = () => {
+  const [child, setChild ] = useState(null);
+
+  useLayoutEffect(() => {
+    setChild(<Typist />);
+  },[]);
+
+  return (
+    <div className='jumbotron jumbotron-fluid text-center bg-transparent vh-75'>
+      <div className='row justify-content-center'>
+        <div className='col-sm-12 pt-4'>
+          <h3 className='pt-4'>Welcome,</h3>
+          <h3>my name is Edward.</h3>
+          <h3>I have experience with</h3>
+        </div>
+        <div className='col-sm-12 pt-4'>
+          <div className='container pt-4'>
+            { child }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Header
-
